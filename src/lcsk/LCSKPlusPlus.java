46457fbda4f87a62daa59ae0ad145d050cadc6d6
Renato Bosnjak <renato.bosnjak@fer.hr>
2019-01-03 13:26:47 +0100

    binary search of G and rest of the algorithm, not tested

--- src/lcsk/LCSKPlusPlus.java
+++ src/lcsk/LCSKPlusPlus.java
@@ -1,5 +1,6 @@
 package lcsk;
 
+import utils.BinarySearch;
 import utils.Pair;
 
 import java.util.*;
@@ -10,13 +11,110 @@ public class LCSKPlusPlus {
 
         // TODO load arguments
         int k = 3;
-        String x = "ABCDEFGCBAKFOWCBAFDACBA";
+        String x = "ABCDE";
         String y = "ABCBA";
+
+        int[][] dp = new int[x.length()][y.length()];
+        int[] maxColDp = new int[x.length()];
 
         ArrayList<MatchPair> matchPairs = findAllKMatchPairs(x, y, k);
 
         ArrayList<Event> events = extractEvents(matchPairs);
         events.sort(new RowMajorEventComparator());
+
+        for (Event event : events) {
+            if (event.getType() == Event.EventType.START) {
+                Pair<Integer, Integer> p = event.getPair();
+                int max = findMaxInArray(maxColDp, 0, p.getSecondElement() + 1);
+                dp[p.getFirstElement()][p.getSecondElement()] = k + max;
+            } else {
+                Pair<Integer, Integer> eventPair = event.getPair();
+                Pair<Integer, Integer> p = new Pair<>(eventPair.getFirstElement() + k, eventPair.getSecondElement() + k);
+
+                // find G such that P continues G -> ip - jp == ig - jg && ip - ig = 1
+                Pair<Integer, Integer> g = findG(p, events);
+                if (g != null) {
+                    // if dp(G) + 1 > dp(P)
+                    int dpOfG = dp[g.getFirstElement()][g.getSecondElement()];
+                    int dpOfP = dp[p.getFirstElement()][p.getSecondElement()];
+                    if(dpOfG + 1> dpOfP){
+                        // dp(P) = dp(G) + 1
+                        dp[p.getFirstElement()][p.getSecondElement()] = dpOfG + 1;
+                    }
+
+                    // if dp(P) > maxColDp(jp + k)
+                    if(dp[p.getFirstElement()][p.getFirstElement()] > maxColDp[p.getSecondElement() + k]) {
+                        // maxColDp(jp + k) = dp(p)
+                        maxColDp[p.getSecondElement() + k] = dp[p.getFirstElement()][p.getFirstElement()];
+                    }
+                }
+            }
+        }
+        int result = findMaxInMatrix(dp);
+        System.out.println(result);
+    }
+
+    /**
+     * Binary search over events, searches for G such that P continues G.
+     *
+     * @return G if it does exist, null otherwise.
+     */
+    private static Pair<Integer, Integer> findG(Pair<Integer, Integer> p, ArrayList<Event> events) {
+        int ip = p.getFirstElement();
+        int jp = p.getSecondElement();
+
+        int ig = ip - 1;
+        int jg = -(ip - jp - ig);
+        Event g = new Event(new Pair<Integer, Integer>(ig, jg), Event.EventType.START);
+
+        RowMajorEventComparator rmec = new RowMajorEventComparator();
+        BinarySearch<Event> bs = new BinarySearch<>(rmec);
+
+        Event[] eventsArray = events.toArray(new Event[events.size()]);
+        int gIndex = bs.binarySearch(eventsArray, g);
+
+        if (gIndex != -1)
+            return g.getPair();
+        else
+            return null;
+    }
+
+    /**
+     * Finds a maximum value in the matrix.
+     */
+    private static int findMaxInMatrix(int[][] matrix) {
+        int max = Integer.MIN_VALUE;
+
+        int numberOfColumns = matrix[0].length;
+        int numberOfRows = matrix.length;
+
+        for(int i = 0; i < numberOfColumns; i++) {
+            for(int j = 0; j < numberOfColumns; j++) {
+                if(matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Finds a maximum value in the array defined as max(array[startingIndex], array[startingIndex+1], ..., array[endingIndex-1])
+     *
+     * @param arr           array to search
+     * @param startingIndex inclusive
+     * @param endingIndex   exclusive
+     * @return maximum value from array slice defined with starting and ending indexes.
+     */
+    private static int findMaxInArray(int[] arr, int startingIndex, int endingIndex) {
+        int max = Integer.MIN_VALUE;
+
+        for (int i = startingIndex; i <= endingIndex; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 
     /**
