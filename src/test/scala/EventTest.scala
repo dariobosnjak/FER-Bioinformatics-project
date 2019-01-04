6224bea39c6655ec217267753cc9a7b2c7c7f654
Dario <dario.bosnjak@fer.hr>
2019-01-04 02:30:31 +0100

    test fir Event and MatchPari

---
+++ src/test/scala/EventTest.scala
@@ -0,0 +1,32 @@
+import org.scalatest.FlatSpec
+
+class EventTest extends FlatSpec{
+  "An Event (1,2)" must "be smaller than Event (1,3)" in {
+    val p = new Event(1,2, Event.START)
+    val g = new Event(1,3, Event.START)
+
+    assert(p < g)
+  }
+
+  "An Event (1,2, END)" must "be smaller than Event (1,3, START)" in {
+    val p = new Event(1,2, Event.END)
+    val g = new Event(1,2, Event.START)
+
+    assert(p < g)
+  }
+
+  "An Event (0,5)" must "be smaller than Event (1,3)" in {
+    val p = new Event(0,5, Event.START)
+    val g = new Event(1,3, Event.END)
+
+    assert(p < g)
+  }
+
+  "An Event (1, 1, START)" must "be equal to Event (1,1, START)" in {
+    val p = new Event(1,1, Event.START)
+    val g = new Event(1,1, Event.START)
+
+    assert(p == g)
+  }
+
+}
