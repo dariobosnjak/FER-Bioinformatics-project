55c96ed6c8599a116637587a21f4495274f5c993
Renato Bosnjak <renato.bosnjak@fer.hr>
2019-01-04 19:37:00 +0100

    Rabin Karp match pair search, not working for bigger k valules

--- src/lcsk/LCSKPlusPlus.java
+++ src/lcsk/LCSKPlusPlus.java
@@ -11,62 +11,57 @@ public class LCSKPlusPlus {
     public static void main(String[] args) throws IOException {
 
         // TODO load arguments
-        int k = 3;
+        int k = 5;
 
         File file = new File("input");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String x = br.readLine();//"ABCDEFGH"; // "ATTATG"
         String y = br.readLine();//"ABCDEFGH"; // "CTATAGAGTA"
 
-        int[][] dp = new int[x.length()][y.length()];
-        // sparse dynamic programming table
-        //HashMap<Pair<Integer, Integer>, Integer> dp = new HashMap<>();
+        // sparse dynamic programming table, key is location in the table
+        HashMap<Pair<Integer, Integer>, Integer> dp = new HashMap<>();
         int[] maxColDp = new int[y.length()];
 
+        // get all k-match pairs between X and Y
         ArrayList<MatchPair> matchPairs = findAllKMatchPairs(x, y, k);
 
+        // all starts and ends of match pairs, sorted in row-major order
         ArrayList<Event> events = extractEvents(matchPairs);
         events.sort(new RowMajorEventComparator());
 
-        int cntr = 0;
         for (Event event : events) {
             if (event.getType() == Event.EventType.START) {
                 Pair<Integer, Integer> p = event.getPair();
-                int max = findMaxInArray(maxColDp, 0, p.getSecondElement() + 1);
-                dp[p.getFirstElement()][p.getSecondElement()] = k + max;
+                int max = findMaxInArray(maxColDp, 0, p.getSecondElement());
+                //System.out.println(max);
+                dp.put(p, k + max);
             } else {
-                // P is END (indexes should be subtracted by one because they are exclusive)
-                Pair<Integer, Integer> eventPair = event.getPair();
-                Pair<Integer, Integer> p = new Pair<>(eventPair.getFirstElement(), eventPair.getSecondElement());
-                // get value of dp at P start position
-                int dpOfPStart = dp[p.getFirstElement() - k][p.getSecondElement() - k];
+                // event.getPair() is an END (indexes should be subtracted by one because they are exclusive)
+                // find starting location of this event
+                Pair<Integer, Integer> pStartLocation = new Pair<>(event.getPair().getFirstElement() - k, event.getPair().getSecondElement() - k);
+                // find GStart such that PStart continues GStart -> ip - jp == ig - jg && ip - ig = 1
+                Pair<Integer, Integer> gStartLocation = findG(pStartLocation, events);
 
-                // find G such that P continues G -> ip - jp == ig - jg && ip - ig = 1
-                // P is END => G is also END
-                Pair<Integer, Integer> g = findG(p, events);
-
-                if (g != null) {
-                    // if dp(G) + 1 > dp(P)
-                    int dpOfGStart = dp[g.getFirstElement() -k][g.getSecondElement() -k];//dp[g.getFirstElement() - 1][g.getSecondElement() - 1];
-
-                    if (dpOfGStart + 1 > dpOfPStart) {
-                        // dp(P) = dp(G) + 1
-                        dp[p.getFirstElement() - k][p.getSecondElement() - k] = dpOfGStart + 1;
+                // if such GStart exists
+                if (gStartLocation != null) {
+                    // if dp(GStart) + 1 > dp(PStart)
+                    if (dp.get(gStartLocation) + 1 > dp.get(pStartLocation)) {
+                        // dp(PStart) = dp(GStart) + 1
+                        dp.put(pStartLocation, dp.get(gStartLocation) + 1);
                     }
                 }
-                // if dp(P) > maxColDp(jp)
-                if (dp[p.getFirstElement() - k][p.getSecondElement() - k] > maxColDp[p.getSecondElement() - k]) {
-                    // maxColDp(jp) = dp(p)
-                    maxColDp[p.getSecondElement() - k] = dp[p.getFirstElement() - k][p.getSecondElement() - k];
-                    //System.out.println(maxColDp[p.getSecondElement() - 1]);
+                // if dp(PStart) > maxColDp(PEnd.j)
+                if (dp.get(pStartLocation) > maxColDp[event.getPair().getSecondElement() - 1]) {
+                    // maxColDp(PEnd.j) = dp(PStart)
+                    maxColDp[event.getPair().getSecondElement() - 1] = dp.get(pStartLocation);
                 }
             }
-            //System.out.println();
-            //printMatrix(dp);
         }
 
-        // TODO - reduce time complexity by saving max value in the loop
-        int result = findMaxInMatrix(dp);
+        int result = 0;
+        if (!dp.values().isEmpty()) {
+            result = Collections.max(dp.values());
+        }
         System.out.println(result);
     }
 
@@ -90,7 +85,7 @@ public class LCSKPlusPlus {
 
         int ig = ip - 1;
         int jg = -(ip - jp - ig);
-        Event g = new Event(new Pair<Integer, Integer>(ig, jg), Event.EventType.END);
+        Event g = new Event(new Pair<Integer, Integer>(ig, jg), Event.EventType.START);
 
         RowMajorEventComparator rmec = new RowMajorEventComparator();
         BinarySearch<Event> bs = new BinarySearch<>(rmec);
@@ -105,25 +100,6 @@ public class LCSKPlusPlus {
     }
 
     /**
-     * Finds a maximum value in the matrix.
-     */
-    private static int findMaxInMatrix(int[][] matrix) {
-        int max = Integer.MIN_VALUE;
-
-        int numberOfColumns = matrix[0].length;
-        int numberOfRows = matrix.length;
-
-        for (int i = 0; i < numberOfRows; i++) {
-            for (int j = 0; j < numberOfColumns; j++) {
-                if (matrix[i][j] > max) {
-                    max = matrix[i][j];
-                }
-            }
-        }
-        return max;
-    }
-
-    /**
      * Finds a maximum value in the array defined as max(array[startingIndex], array[startingIndex+1], ..., array[endingIndex-1])
      *
      * @param arr           array to search
@@ -132,7 +108,7 @@ public class LCSKPlusPlus {
      * @return maximum value from array slice defined with starting and ending indexes.
      */
     private static int findMaxInArray(int[] arr, int startingIndex, int endingIndex) {
-        int max = Integer.MIN_VALUE;
+        int max = 0;
 
         for (int i = startingIndex; i < endingIndex; i++) {
             if (arr[i] > max) {
@@ -167,6 +143,7 @@ public class LCSKPlusPlus {
      * @return all k-match pairs between x and y
      */
     static ArrayList<MatchPair> findAllKMatchPairs(String x, String y, int k) {
+
         ArrayList<MatchPair> matchPairs = new ArrayList<>();
 
         // contains all k-grams from string sequence
@@ -192,32 +169,125 @@ public class LCSKPlusPlus {
                 }
             }
         }
-
         return matchPairs;
     }
 
+    // TODO for bigger k values -> overflow
+    private ArrayList<MatchPair> findAllKMatchPairsRabinKarpSearch(String x, String y, int k){
+        ArrayList<MatchPair> matchPairs2 = new ArrayList<>();
+
+        // large prime number
+        long prime = 7800097;
+        // size of the alphabet
+        String xDistinct = x.chars().distinct().toString();
+        String yDistinct = y.chars().distinct().toString();
+        long alphabetSize = xDistinct.concat(yDistinct).chars().distinct().count();
+
+        // key is hashcode, value is starting index of substring in X and Y (Pair object)
+        HashMap<Long, ArrayList<Pair<Integer, Integer>>> kGramMapX = new HashMap<>();
+
+        // calculate hash for the first kGram in X (different method is used for calculation of others)
+        long hash = 0;
+        for (int i = 0; i < k; i++) {
+            hash += (x.charAt(i) * Math.pow(alphabetSize, k - i - 1)) % prime;
+        }
+        // if key does not exist, create new map entry
+        kGramMapX.computeIfAbsent(hash, k1 -> new ArrayList<>());
+        // this kGram with hashcode "hash" starts at index 0 for string X (starting index for Y is unknown for now)
+        kGramMapX.get(hash).add(new Pair<Integer, Integer>(0, null));
+
+        // calculate hash for other kGrams
+        for (int startX = 1; startX < x.length() - (k - 1); startX++) {
+            hash = ((hash - x.charAt(startX - 1) * (long) Math.pow(alphabetSize, k - 1)) * alphabetSize + x.charAt(startX + k - 1)) % prime;
+            hash = hash < 0 ? hash + prime : hash;
+            //hash = (hash + prime - ((long) Math.pow(alphabetSize, k - 1) * x.charAt(startX - 1) % prime)) % prime;
+            //hash = (hash * alphabetSize + x.charAt(startX + k - 1)) % prime;
+
+            if("JGABCDEFGJFDLKHFDJSH".equals(x.substring(startX, startX+k))){
+                System.out.println(hash);
+            }
+
+            // if key does not exist, create new map entry
+            kGramMapX.computeIfAbsent(hash, k1 -> new ArrayList<>());
+            kGramMapX.get(hash).add(new Pair<>(startX, null));
+        }
+
+        // calculate hash for the first kGram in Y (different method is used for calculation of others)
+        hash = 0;
+        for (int i = 0; i < k; i++) {
+            hash += (y.charAt(i) * Math.pow(alphabetSize, k - i - 1)) % prime;
+        }
+        // if key exists -> intersection -> update result
+        ArrayList<Pair<Integer, Integer>> startingLocationsInX = kGramMapX.get(hash);
+        if (startingLocationsInX != null) {
+            for (Pair<Integer, Integer> p : startingLocationsInX) {
+                // if hashcode is the same, substrings can be equal - check if they are
+                if (x.substring(p.getFirstElement(), p.getFirstElement() + k).equals(y.substring(0, k))) {
+                    Pair<Integer, Integer> pCopy = new Pair<>(p.getFirstElement(), 0);
+                    Pair<Integer, Integer> end = new Pair<>(pCopy.getFirstElement() + k, pCopy.getSecondElement() + k);
+                    matchPairs2.add(new MatchPair(pCopy, end));
+                }
+            }
+        }
+
+        // calculate hash for other kGrams
+        for (int startY = 1; startY < y.length() - (k - 1); startY++) {
+            //hash = ((hash - y.charAt(startY - 1) * (long) Math.pow(alphabetSize, k - 1)) * alphabetSize + y.charAt(startY + k - 1)) % prime;
+            //hash = hash < 0 ? hash + prime : hash;
+            hash = (hash + prime - (long) Math.pow(alphabetSize, k - 1) * y.charAt(startY - 1) % prime) % prime;
+            hash = (hash * alphabetSize + y.charAt(startY + k - 1)) % prime;
+
+            if("JGABCDEFGJFDLKHFDJSH".equals(y.substring(startY, startY+k))){
+                System.out.println(hash);
+                System.out.println();
+            }
+
+            // if key exists -> intersection -> update result
+            startingLocationsInX = kGramMapX.get(hash);
+            if (startingLocationsInX != null) {
+                for (Pair<Integer, Integer> p : startingLocationsInX) {
+                    // if hashcode is the same, substrings can be equal - check if they are
+                    //System.out.println(x.substring(p.getFirstElement(), p.getFirstElement() + k));
+                    //System.out.println((y.substring(startY, startY + k)));
+                    //System.out.println();
+                    if (x.substring(p.getFirstElement(), p.getFirstElement() + k).equals(y.substring(startY, startY + k))) {
+                        Pair<Integer, Integer> pCopy = new Pair<>(p.getFirstElement(), startY);
+                        Pair<Integer, Integer> end = new Pair<>(pCopy.getFirstElement() + k, pCopy.getSecondElement() + k);
+                        matchPairs2.add(new MatchPair(pCopy, end));
+                    }
+                }
+            }
+
+        }
+
+        return matchPairs2;
+    }
+
     /**
-     * Creates new map where keys are k-grams from the given string, and values are utils.Pair objects. utils.Pair objects represent
-     * beginning and ending indexes of k-gram in the string. Each k-gram can appear multiple times in the string.
+     * Creates new map where keys are k-grams from the given string, and values are utils.Pair objects. They represent
+     * beginning (inclusive) and ending (exclusive) indexes of k-gram in the string.
+     * Each k-gram can appear multiple times in the string.
      *
      * @param s string
      * @param k defines the length of k-grams
      */
     private static HashMap<String, ArrayList<Pair<Integer, Integer>>> initializeKGramMap(String s, int k) {
         int sLength = s.length();
-        HashMap<String, ArrayList<Pair<Integer, Integer>>> kGramMap = new HashMap<String, ArrayList<Pair<Integer, Integer>>>();
+        HashMap<String, ArrayList<Pair<Integer, Integer>>> kGramMap = new HashMap<>();
 
         for (int start = 0; start < sLength - (k - 1); start++) {
+            // end is exclusive
             int end = start + k;
 
-            Pair<Integer, Integer> p = new Pair<>(start, end);
             String kGram = s.substring(start, end);
-
-            ArrayList<Pair<Integer, Integer>> value;
-            value = kGramMap.getOrDefault(kGram, new ArrayList<>());
-            value.add(p);
-
-            kGramMap.put(kGram, value);
+            // if key exists -> append new pair
+            if (kGramMap.get(kGram) != null) {
+                kGramMap.get(kGram).add(new Pair<>(start, end));
+            } else {
+                // if key does not exists -> create new arraylist and append pair
+                kGramMap.put(kGram, new ArrayList<Pair<Integer, Integer>>());
+                kGramMap.get(kGram).add(new Pair<>(start, end));
+            }
         }
 
         return kGramMap;
