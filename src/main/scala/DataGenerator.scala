bbf872579f4eed6c5c975fabf534908f3897825c
Dario <dario.bosnjak@fer.hr>
2019-01-05 11:14:41 +0100

    DataGenerator - FASTA format

---
+++ src/main/scala/DataGenerator.scala
@@ -0,0 +1,32 @@
+import java.io._
+
+import scala.util.Random
+
+object DataGenerator {
+  val MAX_LINE_LENGTH = 80
+
+  def main(args: Array[String]): Unit = {
+    val file = args(0)
+    val length: Int = args(1).toInt
+
+    val alphabet = Array("A", "C", "G", "T")
+    val random = new Random(System.currentTimeMillis())
+    val header = ">sequence-"
+
+    val pw = new PrintWriter(new File(file))
+    for (seqId <- Seq(0, 1)) {
+      pw.write(header + seqId)
+      pw.write(System.lineSeparator())
+      for (i <- 0 until length) {
+        if (i != 0 && i % MAX_LINE_LENGTH == 0) {
+          pw.write(System.lineSeparator())
+        }
+        pw.write(alphabet(random.nextInt(alphabet.length)))
+      }
+      pw.write(System.lineSeparator())
+    }
+
+    pw.flush()
+    pw.close()
+  }
+}
