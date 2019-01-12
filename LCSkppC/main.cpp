956bc0b4342b7ea51de621add2d967603bc8d9d2
dorian.ljubenko@gmail.com <dorian.ljubenko@gmail.com>
2019-01-12 19:19:02 +0100

    Changes and comments

--- LCSkppC/main.cpp
+++ LCSkppC/main.cpp
@@ -12,19 +12,21 @@ FER, Bioinformatics project
 int main(int argc, char *argv[]) {
 	
 	if (argc!=3 ) {
-		std::cerr << "Error! Number of arguments has to be 3!" << std::endl;
+		std::cerr << "Error! Correct function call:LSCkppC   value of k  file name" << std::endl;
 		return -1;
 	}
 	
 	bool firstString = true;
 	bool firstLine = true;
+	int k = atoi(argv[1]);
+	int solution;
+	
+	std::cout << "k=" << k << std::endl;
+	
+	//Reading sekuences s1 and s2 from file
 	std::string s1 = "", s2 = "";
 	std::ifstream input(argv[2]);
-	int k = atoi(argv[1]);
-	std::cout << "k=" << k << std::endl;
 
-
-	//Reading sekuences s1 and s2 from file
 	for (std::string line; getline(input, line); ) {
 		if (firstLine) {
 			firstLine = false;
@@ -42,8 +44,10 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	//Initialising class lckpp and calling run function
 	LCKPP lckpp;
-	std::cout << "Final solution max Dp(P)= " << lckpp.run(k,s1, s2) << std::endl;
+	solution = lckpp.run(k, s1, s2);
+	std::cout << "max Dp(P)= " << solution << std::endl;
 
 	return 0;
 }
