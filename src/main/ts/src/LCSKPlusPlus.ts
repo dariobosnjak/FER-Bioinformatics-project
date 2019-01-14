d4d9690e703ac35e291e124c082c9693575868d0
Matija Marić <sirSlani@gmail.com>
2019-01-14 03:48:16 +0100

    Razuman hash

--- src/main/ts/src/LCSKPlusPlus.ts
+++ src/main/ts/src/LCSKPlusPlus.ts
@@ -1,12 +1,12 @@
 import { matchPairs } from "./matchPairs";
-import { EventType, Event } from "./interfaces";
+import { EventType, Event, Pair } from "./interfaces";
 import * as _ from "lodash";
 import { eventComparator } from "./util";
 import * as wu from "wu";
 import FenwickTree from "./fenwickTree";
 
 export function lcskPlusPlus(seqA: string, seqB: string, k: number): number {
-  const dp = new Map<string, number>();
+  const dp = new Map<number, number>();
   const maxColDp = new FenwickTree(seqB.length);
 
   const mPairs = matchPairs(seqA, seqB, k);
@@ -24,7 +24,7 @@ export function lcskPlusPlus(seqA: string, seqB: string, k: number): number {
 
   events.forEach(event => {
     if (event.type === EventType.Start) {
-      dp.set(s(event.pair), k + (maxColDp.query(event.pair.j) || 0));
+      dp.set(hash(event.pair), k + (maxColDp.query(event.pair.j) || 0));
     } else {
       const p: Event = {
         pair: { i: event.pair.i - k, j: event.pair.j - k },
@@ -34,10 +34,13 @@ export function lcskPlusPlus(seqA: string, seqB: string, k: number): number {
         pair: { i: p.pair.i - 1, j: p.pair.j - 1 },
         type: EventType.Start
       };
-      if (dp.has(s(g.pair))) {
-        dp.set(s(p.pair), Math.max(dp.get(s(p.pair)), dp.get(s(g.pair)) + 1));
+      if (dp.has(hash(g.pair))) {
+        dp.set(
+          hash(p.pair),
+          Math.max(dp.get(hash(p.pair)), dp.get(hash(g.pair)) + 1)
+        );
       }
-      maxColDp.update(event.pair.j, dp.get(s(p.pair)));
+      maxColDp.update(event.pair.j, dp.get(hash(p.pair)));
     }
   });
 
@@ -47,6 +50,6 @@ export function lcskPlusPlus(seqA: string, seqB: string, k: number): number {
   return 0;
 }
 
-function s(obj: any) {
-  return JSON.stringify(obj);
+function hash(obj: Pair) {
+  return obj.i * 1610612741 + obj.j;
 }
